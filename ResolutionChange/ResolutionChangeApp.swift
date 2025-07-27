0f61b51ecb1e9a88951d3753213f4fa7e75589d2
cassaicus <cassaicus+g@gmail.com>
2025-07-27 17:35:25 +0900

    3rd

--- ResolutionChange/ResolutionChangeApp.swift
+++ ResolutionChange/ResolutionChangeApp.swift
@@ -1,16 +1,13 @@
-
-
 import SwiftUI
 
 
 @main
 struct ResolutionChangeApp: App {
     
-    
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
-        WindowGroup {
+        Settings {
             ContentView()
         }
     }

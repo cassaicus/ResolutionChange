fd9c95868577194cd04cefb70386bb0909ba6390
cassaicus <cassaicus+g@gmail.com>
2025-07-27 17:28:03 +0900

    Initial Commit

---
+++ ResolutionChange/ResolutionChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResolutionChangeApp.swift
+//  ResolutionChange
+//
+//  Created by ibis on 2025/07/27.
+//
+
+import SwiftUI
+
+@main
+struct ResolutionChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
